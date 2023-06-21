5313aa8f10d02937f931b2757e5c4c9df859d374
sajid <mohdsajid8010@gmail.com>
2023-06-21 11:56:28 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,50 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [gender, setGender] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h2>Select your gender:</h2>
+      <input type="radio" id="male" name="gender" value="male"
+        onClick={(e) => setGender(e.target.value)} />
+      <label for="male">Male</label>
+      <input type="radio" id="female" name="gender" value="female"
+        onClick={(e) => setGender(e.target.value)} />
+      <label for="female">Female</label>
+
+      {
+        gender && (
+          gender === "male" ?
+            (
+              <div>
+                <h2>Select your shirt size:</h2>
+                <select name="shirt" id="shirt" >
+                  <option value="">Select Size</option>
+                  <option value="small">Small</option>
+                  <option value="medium">Medium</option>
+                  <option value="large">large</option>
+                </select>
+              </div>
+            )
+            :
+            (
+              <div>
+                <h2>Select your dress size:</h2>
+                <select name="dress" id="dress" >
+                  <option value="">Select Size</option>
+                  <option value="2">2</option>
+                  <option value="4">4</option>
+                  <option value="6">6</option>
+                </select>
+
+              </div>
+            )
+        )
+      }
     </div>
   )
 }
